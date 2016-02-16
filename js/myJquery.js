e0cd8ad42cbb16dd69d91637574e65a215fa37f6
Chethan Bhat <chethanbhat@gmail.com>
2016-02-16 18:47:54 +0530

    Changes to MyJquery JS File

--- js/myJquery.js
+++ js/myJquery.js
@@ -1,16 +1,29 @@
 $(document).ready(function() {
 	$.get("includes/header.inc.html", function(data) {
 		$("#header").html(data);
+	
+		var url = window.location;
+		// Will only work if string in href matches with location
+		$('ul.nav a[href="'+ url +'"]').parent().addClass('active');
+
+		// Will also work for relative and absolute hrefs
+		$('ul.nav a').filter(function() {
+		return this.href == url;
+		}).parent().addClass('active');
+		
 	});
 	
 	$.get("includes/footer.inc.html", function(data) {
 		$("#footer").html(data);
 	});
+	
+	
 
-
+	
+	/*
 	$("li a").click (function (e) {
 		$(this).addClass('active').siblings().removeClass('active');
-	});
+	}); */
 
 });
 
